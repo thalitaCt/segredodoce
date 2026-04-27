adca1aff27b95e4298256b1a44f139a6820b00c8
thalitaCt <costathalita685@gmail.com>
2026-04-27 10:38:29 -0300

    Update conexao.php

--- includes/conexao.php
+++ includes/conexao.php
@@ -3,17 +3,11 @@
     $db = "neondb";
     $user = "neondb_owner";
     $pass = "npg_PpQ23OwtrDxL";
-    $port = "5432";
-
-    $endpoint = "ep-winter-wave-acvep69a";
-
-    $dns = "pgsql:host=$host;port=$port;dbname=$db;sslmode=require;options=endpoint=$endpoint";
 
     try {
-        $pdo = new PDO($dns, $user, $pass, [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_EMULATE_PREPARES => false
+        $pdo = new PDO("pgsql:host=$host;port=5432;dbname=$db;sslmode=require", $user, $pass, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]);
-        echo "Conectou!";
     } catch (PDOException $e) {
         echo "Erro: " . $e->getMessage();
     }
