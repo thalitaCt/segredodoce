8c5e96d06f9a4cf5271201861f06b1c4c24dfc23
thalitaCt <costathalita685@gmail.com>
2026-05-03 20:13:00 -0300

    atualizacao

--- admin.php
+++ admin.php
@@ -3,10 +3,14 @@ session_start();
 include 'includes/conexao.php';
 
 
+
+
 if (!isset($_SESSION['usuario']) || $_SESSION['tipo'] != 'admin') {
     header("Location: index.php");
     exit;
 }
+
+
 
 
 /* TOTAL CLIENTES */
@@ -14,14 +18,20 @@ $sqlClientes = $pdo->query("SELECT COUNT(*) AS total FROM clientes");
 $totalClientes = $sqlClientes->fetch(PDO::FETCH_ASSOC)['total'];
 
 
+
+
 /* TOTAL PEDIDOS */
 $sqlPedidos = $pdo->query("SELECT COUNT(*) AS total FROM pedidos");
 $totalPedidos = $sqlPedidos->fetch(PDO::FETCH_ASSOC)['total'];
 
 
+
+
 /* TOTAL VENDIDO */
 $sqlVendas = $pdo->query("SELECT COALESCE(SUM(total),0) AS total FROM pedidos");
 $totalVendas = $sqlVendas->fetch(PDO::FETCH_ASSOC)['total'];
+
+
 
 
 /* ULTIMOS CLIENTES */
@@ -31,6 +41,8 @@ $clientes = $pdo->query("
     ORDER BY id_clientes DESC
     LIMIT 5
 ")->fetchAll(PDO::FETCH_ASSOC);
+
+
 
 
 /* ULTIMOS PEDIDOS */
@@ -50,19 +62,27 @@ $pedidos = $pdo->query("
  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 <title>Painel Admin</title>
 <style>
-    
+   
 </style>
 </head>
 <body>
 
 
+
+
 <div class="admin-container">
+
+
 
 
 <h1 class="titulo">Painel Administrativo</h1>
 
 
+
+
 <div class="cards">
+
+
 
 
 <div class="card">
@@ -71,10 +91,14 @@ $pedidos = $pdo->query("
 </div>
 
 
+
+
 <div class="card">
 <h3>Pedidos</h3>
 <p><?= $totalPedidos ?></p>
 </div>
+
+
 
 
 <div class="card">
@@ -83,14 +107,22 @@ $pedidos = $pdo->query("
 </div>
 
 
+
+
 </div>
+
+
 
 
 <div class="grid">
 
 
+
+
 <div class="box">
 <h2>Últimos Clientes</h2>
+
+
 
 
 <?php foreach($clientes as $c): ?>
@@ -101,11 +133,17 @@ $pedidos = $pdo->query("
 <?php endforeach; ?>
 
 
+
+
 </div>
+
+
 
 
 <div class="box">
 <h2>Últimos Pedidos</h2>
+
+
 
 
 <?php foreach($pedidos as $p): ?>
@@ -117,13 +155,21 @@ Pedido #<?= $p['id_pedidos'] ?><br>
 <?php endforeach; ?>
 
 
-</div>
 
 
 </div>
 
 
+
+
 </div>
+
+
+
+
+</div>
+
+
 
 
 </body>
