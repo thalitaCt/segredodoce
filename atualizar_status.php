c8a837c48217bf6b4127eafe36bc70d475e98921
thalitaCt <costathalita685@gmail.com>
2026-05-14 12:05:58 -0300

    Update atualizar_status.php

--- atualizar_status.php
+++ atualizar_status.php
@@ -1,14 +1,32 @@
 <?php
+session_start();
 include 'includes/conexao.php';
 
+if(!isset($_SESSION['tipo']) || $_SESSION['tipo'] != 'recepcionista'){
+    header("Location: login.php");
+    exit;
+}
 
-$id = $_POST['id'];
-$status = $_POST['status'];
+$id = $_POST['id'] ?? null;
+$status = $_POST['status'] ?? null;
 
+$statusesValidos = [
+    'Pedido Confirmado',
+    'Pendente',
+    'Em preparo',
+    'Pronto',
+    'Entregue',
+    'Cancelado'
+];
+
+if(!$id || !in_array($status, $statusesValidos)){
+    header("Location: atendente.php?msg=erro_status");
+    exit;
+}
 
 $sql = $pdo->prepare("UPDATE pedidos SET status = ? WHERE id_pedidos = ?");
 $sql->execute([$status, $id]);
 
-
-header("Location: atendente.php");
+header("Location: atendente.php?msg=status_atualizado");
 exit;
+?>
