96f6dec135b56f44b78231faae0265bc2b43457f
thalitaCt <costathalita685@gmail.com>
2026-05-14 10:53:41 -0300

    atualizacao

--- cadastro.php
+++ cadastro.php
@@ -116,5 +116,22 @@ senha.addEventListener('input', verificarSenha);
 confirmar.addEventListener('input', verificarSenha);
 </script>
 
+
+<script>
+function toggleSenha(id, icon) {
+    const input = document.getElementById(id);
+
+    if (input.type === "password") {
+        input.type = "text";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+    } else {
+        input.type = "password";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+    }
+}
+</script>
+
 </body>
 </html>
