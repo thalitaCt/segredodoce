e7c6e049edd1d61dfc1bb29460ea0d0a2e7d2199
thalitaCt <costathalita685@gmail.com>
2026-05-14 14:02:17 -0300

    Update editar.php

--- clientes/editar.php
+++ clientes/editar.php
@@ -51,12 +51,37 @@ if ($_GET['erro'] == 'telefone_invalido') echo "Número de celular inválido. Us
 <input type="text" name="bairro" value="<?= $cliente['bairro'] ?>" placeholder="Bairro">
 <input type="text" name="cidade" value="<?= $cliente['cidade'] ?>" placeholder="Cidade">
 
-<select name="estado">
-    <option value="">Estado</option>
-    <option value="RJ" <?= $cliente['estado']=='RJ'?'selected':'' ?>>Rio de Janeiro</option>
-    <option value="SP" <?= $cliente['estado']=='SP'?'selected':'' ?>>São Paulo</option>
-    <option value="MG" <?= $cliente['estado']=='MG'?'selected':'' ?>>Minas Gerais</option>
-</select>
+<select name="estado" required>
+    <option value="">Selecione o estado</option>
+
+    <option value="AC" <?= $cliente['estado']=='AC'?'selected':'' ?>>Acre (AC)</option>
+    <option value="AL" <?= $cliente['estado']=='AL'?'selected':'' ?>>Alagoas (AL)</option>
+    <option value="AP" <?= $cliente['estado']=='AP'?'selected':'' ?>>Amapá (AP)</option>
+    <option value="AM" <?= $cliente['estado']=='AM'?'selected':'' ?>>Amazonas (AM)</option>
+    <option value="BA" <?= $cliente['estado']=='BA'?'selected':'' ?>>Bahia (BA)</option>
+    <option value="CE" <?= $cliente['estado']=='CE'?'selected':'' ?>>Ceará (CE)</option>
+    <option value="DF" <?= $cliente['estado']=='DF'?'selected':'' ?>>Distrito Federal (DF)</option>
+    <option value="ES" <?= $cliente['estado']=='ES'?'selected':'' ?>>Espírito Santo (ES)</option>
+    <option value="GO" <?= $cliente['estado']=='GO'?'selected':'' ?>>Goiás (GO)</option>
+    <option value="MA" <?= $cliente['estado']=='MA'?'selected':'' ?>>Maranhão (MA)</option>
+    <option value="MT" <?= $cliente['estado']=='MT'?'selected':'' ?>>Mato Grosso (MT)</option>
+    <option value="MS" <?= $cliente['estado']=='MS'?'selected':'' ?>>Mato Grosso do Sul (MS)</option>
+    <option value="MG" <?= $cliente['estado']=='MG'?'selected':'' ?>>Minas Gerais (MG)</option>
+    <option value="PA" <?= $cliente['estado']=='PA'?'selected':'' ?>>Pará (PA)</option>
+    <option value="PB" <?= $cliente['estado']=='PB'?'selected':'' ?>>Paraíba (PB)</option>
+    <option value="PR" <?= $cliente['estado']=='PR'?'selected':'' ?>>Paraná (PR)</option>
+    <option value="PE" <?= $cliente['estado']=='PE'?'selected':'' ?>>Pernambuco (PE)</option>
+    <option value="PI" <?= $cliente['estado']=='PI'?'selected':'' ?>>Piauí (PI)</option>
+    <option value="RJ" <?= $cliente['estado']=='RJ'?'selected':'' ?>>Rio de Janeiro (RJ)</option>
+    <option value="RN" <?= $cliente['estado']=='RN'?'selected':'' ?>>Rio Grande do Norte (RN)</option>
+    <option value="RS" <?= $cliente['estado']=='RS'?'selected':'' ?>>Rio Grande do Sul (RS)</option>
+    <option value="RO" <?= $cliente['estado']=='RO'?'selected':'' ?>>Rondônia (RO)</option>
+    <option value="RR" <?= $cliente['estado']=='RR'?'selected':'' ?>>Roraima (RR)</option>
+    <option value="SC" <?= $cliente['estado']=='SC'?'selected':'' ?>>Santa Catarina (SC)</option>
+    <option value="SP" <?= $cliente['estado']=='SP'?'selected':'' ?>>São Paulo (SP)</option>
+    <option value="SE" <?= $cliente['estado']=='SE'?'selected':'' ?>>Sergipe (SE)</option>
+    <option value="TO" <?= $cliente['estado']=='TO'?'selected':'' ?>>Tocantins (TO)</option>
+    </select>
 
 <select name="regiao">
     <option value="">Região</option>
