2a1c2641914ffe0a0f52e41ba845d10b38ef63fc
thalitaCt <costathalita685@gmail.com>
2026-06-02 07:00:06 -0300

    Update criar.php

--- clientes/criar.php
+++ clientes/criar.php
@@ -317,12 +317,14 @@ function verificarEntrega(){
         estadoValor === 'RJ'
     ){
 
-        regiao.disabled = false;
+        regiao.style.pointerEvents = 'none';
+        regiao.style.opacity = '0.7';
 
     } else {
 
         regiao.value = 'Entrega Externa';
-        regiao.disabled = true;
+        regiao.style.pointerEvents = 'auto';
+        regiao.style.opacity = '1';
 
     }
 
